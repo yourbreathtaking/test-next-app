f0fb14b0f70d7521c21bd8ae24b18c22b3222854
yourbreathtaking <52409746+yourbreathtaking@users.noreply.github.com>
2024-12-01 16:16:44 +0100

    Update page.tsx

--- app/(root)/why/page.tsx
+++ app/(root)/why/page.tsx
@@ -1,7 +1,5 @@
 export default async function Page() {
-    const data = await new Promise((resolve) =>
-      setTimeout(() => resolve('Hello World!'), 2000) // Simulate loading
-    );
+    
   
     return (<div>who ceares</div>);
   }
